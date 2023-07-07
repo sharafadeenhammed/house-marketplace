3c2b9c96af5874cf5a7e524db0d43d9b0c5971ce
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-07-07 20:48:32 +0100

    createlisting page layot completed with selected image preview

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import "react-toastify/dist/ReactToastify.css"
-import { ToastContainer } from "react-toastify"
-import Explore from "./pages/Explore"
-import SignUp from "./pages/SignUp"
-import SignIn from "./pages/SignIn"
-import Profile from "./pages/Profile"
-import Offers from "./pages/Offers"
-import ForgotPassword from "./pages/ForgotPassword"
-import Category from "./pages/Category"
-import NotFoundPage from "./pages/NotFoundPage"
-import NavBar from "./components/NavBar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Explore from "./pages/Explore";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import Category from "./pages/Category";
+import NotFoundPage from "./pages/NotFoundPage";
+import NavBar from "./components/NavBar";
+import CreateListing from "./pages/CreateListing";
 const toastOptions = {
   position: "top-right",
   autoClose: 5000,
@@ -19,23 +20,24 @@ const toastOptions = {
   draggable: true,
   progress: undefined,
   theme: "light",
-  }
+};
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Explore />} />
-          <Route path="/offers" element={<Offers/>} />
-          <Route path="/category/:categoryName" element={<Category/>} />
-          <Route path="/sign-in" element={<SignIn/>} />
-          <Route path="/sign-up" element={<SignUp/>} />
-          <Route path="/profile" element={<Profile/>} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
         <NavBar />
-        <ToastContainer/>
+        <ToastContainer />
       </Router>
     </>
   );
